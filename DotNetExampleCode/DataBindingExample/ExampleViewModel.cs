249541ef019011b0636d7ed893a42d5d4b60d218
Manoj Patel <sw@mpvisual.com>
2013-10-17 20:33:42 -0400

    DataBinding Basics

---
+++ DotNetExampleCode/DataBindingExample/ExampleViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingExample
+{
+    public class ExampleViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<string> NickNames { get; set; }
+
+        public ExampleViewModel()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+
+        public ExampleViewModel(string first, string last, List<string> nickNames)
+        {
+            FirstName = first;
+            LastName = last;
+            NickNames = nickNames;
+        }
+    }
+}
---
+++ DotNetExampleCode/DataBindingExample/ToUppercaseConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DataBindingExample
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class ToUppercaseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string fromValue = value.ToString();
+            return fromValue.ToUpper();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+       
+    }
+}
